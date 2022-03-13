46059a01a43b19f0b59442abc185c120c6d1ba11
Maddy <madisonschaaf48@gmail.com>
2022-03-13 10:30:06 -0600

    first

---
+++ potential-enigma-main/Develop/index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+    //add inquirer
+// TODO: Create an array of questions for user input
+    //what was your motivation?
+    //why did you build this project?
+    //What porblem does it solve?
+    //what did you learn?
+    //what makes your project stand out?
+    //challenges
+    //future implementation?
+
+const questions = [];
+
+// TODO: Create a function to write README file
+    //generate?
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
