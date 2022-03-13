b7f062987b37de9fd185671e91b4e64bb635af1d
Maddy <madisonschaaf48@gmail.com>
2022-03-13 14:29:52 -0600

    added questions for readMe info

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
     //add inquirer
+    const inquirer = require('inquirer');
+   // const fs = require('fs');
 // TODO: Create an array of questions for user input
     //what was your motivation?
     //why did you build this project?
@@ -8,6 +10,55 @@
     //what makes your project stand out?
     //challenges
     //future implementation?j
+
+//questions i am adding without using the const question [] provided
+
+const promptUser = () => {
+return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'project title',
+        message: 'What is the title of your project?'
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project?'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install this project? git clone and then npm install'
+    },
+    {
+        type: 'input',
+        name: 'usage information',
+        message: 'What will you use this for?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Who contributed to this project?'
+    },
+    {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'What licenses were used? (Check all the apply)',
+        choices: ['inquirer', 'idk']
+    }, 
+    {
+        type: 'input',
+        name: 'GitHub username',
+        message: 'What is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email'
+    }
+])
+}
+promptUser().then(answers => console.log(answers))
 
 const questions = [];
 
