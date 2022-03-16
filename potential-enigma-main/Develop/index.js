1895add2fd54e48c2e52c9e2e2a790a9aa281c31
Maddy <madisonschaaf48@gmail.com>
2022-03-16 17:19:02 -0600

    generated read me file using writefile system

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-    //add inquirer
     const inquirer = require('inquirer');
-   // const fs = require('fs');
+    const generateMarkdown = require('./utils/generateMarkdown')
+    const fs = require('fs');
 // TODO: Create an array of questions for user input
     //what was your motivation?
     //why did you build this project?
@@ -13,32 +13,64 @@
 
 //questions i am adding without using the const question [] provided
 
-const promptUser = () => {
+const promptUser = readMeData => {
 return inquirer.prompt([
     {
         type: 'input',
-        name: 'project title',
-        message: 'What is the title of your project?'
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if(titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the Read Me title.');
+                return false;
+            }
+        }
     }, 
     {
         type: 'input',
         name: 'description',
-        message: 'Describe your project?'
+        message: 'Describe your project? (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project.');
+                return false;
+            }      
+        } 
     },
     {
         type: 'input',
         name: 'install',
-        message: 'How do you install this project? git clone and then npm install'
+        message: 'How do you install this project? git clone and then npm install',
     },
     {
         type: 'input',
         name: 'usage information',
-        message: 'What will you use this for?'
+        message: 'What will you use this for? (Required)',
+        validate: usage => {
+            if(usage) {
+                return true;
+            } else {
+                console.log('Please enter the use of this project?');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'contribution',
-        message: 'Who contributed to this project?'
+        message: 'Who contributed to this project? (Required)',
+        validate: contribution => {
+            if(contribution) {
+                return true;
+            } else {
+                console.log('Please input who contributed to this project.');
+                return false;
+            }
+        }
     },
     {
         type: 'checkbox',
@@ -49,26 +81,80 @@ return inquirer.prompt([
     {
         type: 'input',
         name: 'GitHub username',
-        message: 'What is your github username?'
+        message: 'What is your github username? (Required)',
+        validate: userName => {
+            if(userName) {
+                return true;
+            } else {
+                console.log('Please provide your username.')
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email'
+        message: 'What is your email? (Required)',
+        validate: email => {
+            if(email) {
+                return true;
+            } else {
+                console.log('Please provide your email.')
+                return false;
+            }
+        }
     }
 ])
 }
-promptUser().then(answers => console.log(answers))
 
-const questions = [];
+
+
+
+// check this part out at 9.3.5 also if you want to add another read me-- confirming things on 9.3.6 using when
+//const questions = [];
+
+//create the function and then call it using the promise? this write file should look the the generate site write file.
 
 // TODO: Create a function to write README file
-    //generate?
-function writeToFile(fileName, data) {}
+    //generate?    data needs to be the parameter to gather the data
+
+
+promptUser()
+.then(readMeData => {
+    const readMePage = generateMarkdown(readMeData);
+    fs.writeFile('./ReadMe.md', readMePage, err => {
+    if (err) throw new Error (err)
+    console.log('read me updated')
+})
+})
+
+
+
+
+//THIS CODE IS WRITING TO READ ME!
+    // promptUser()
+// .then(data => {
+//     const filename = 'ReadMe.md'
+//     //function writeToFile(filename, data) {
+//         fs.writeFile(filename, JSON.stringify(data, null, '\t'), err =>
+//         err ? console.log(err): console.log('success!')
+//         )
+      
+//       })
+  //}
+
+
+//writeToFile()
 
 // TODO: Create a function to initialize app
 function init() {}
+// promptUser()
+// .then(answers => console.log(answers))
+// //writeToFile("readme.md", generateMarkdown(promptUser))
+// }
 
 // Function call to initialize app
 init();
+
+
 
--- potential-enigma-main/Develop/utils/generateMarkdown.js
+++ potential-enigma-main/Develop/utils/generateMarkdown.js
@@ -1,14 +1,17 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {
+// }
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -17,4 +20,5 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+//this is only the generate  markdown function 
+module.exports = generateMarkdown
